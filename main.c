7596f106861aee095c6e034da2954f97fed65970
Zedmous <zedmous@gmail.com>
2024-07-08 16:54:19 -0400

    imprimir mensajes

--- main.c
+++ main.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-// Función para imprimir un mensaje
+// Funciï¿½n para imprimir un mensaje
 void imprimirMensaje(const char* mensaje) {
     printf("%s\n", mensaje);
 }
 
-// Función para imprimir una cadena de caracteres
+// Funciï¿½n para imprimir una cadena de caracteres
 void imprimirString(const char* mensaje, const char* dato) {
     printf("%s%s\n", mensaje, dato);
 }
 
-// Función para imprimir un entero
+// Funciï¿½n para imprimir un entero
 void imprimirEntero(const char* mensaje, int dato) {
     printf("%s%d\n", mensaje, dato);
 }
 
-// Función para imprimir un flotante
+// Funciï¿½n para imprimir un flotante
 void imprimirFloat(const char* mensaje, float dato) {
     printf("%s%f\n", mensaje, dato);
 }
 
-// Función para leer una cadena de caracteres
+// Funciï¿½n para leer una cadena de caracteres
 void leerString(const char* mensaje, char* r) {
     printf("%s", mensaje);
     scanf("%s", r);
 }
 
-// Función para leer un flotante
+// Funciï¿½n para leer un flotante
 float leerFloat(const char* mensaje) {
     float r = 0.0;
     printf("%s", mensaje);
@@ -35,7 +35,7 @@ float leerFloat(const char* mensaje) {
     return r;
 }
 
-// Función para leer un entero
+// Funciï¿½n para leer un entero
 int leerEntero(const char* mensaje) {
     int r = 0;
     printf("%s", mensaje);
@@ -49,22 +49,22 @@ float calcularSuma(float a, float b) {
 }
 
 int main(int argc, char *argv[]) {
-    printf("----------INICIANDO PROGRAMA----------\n");
-    printf("--------------------------------------\n");
+    imprimirMensaje("----------INICIANDO PROGRAMA----------\n");
+    imprimirMensaje("--------------------------------------\n");
 	float numero1 = 0.0;
     float numero2 = 0.0;
-    printf("----------PASO 1----------\n");
-    printf("----------LECTURA DE DATOS----------\n");
+    imprimirMensaje("----------PASO 1----------\n");
+    imprimirMensaje("----------LECTURA DE DATOS----------\n");
 	numero1 = leerFloat("Ingrese el numero 1: ");
     numero2 = leerFloat("Ingrese el numero 2: ");
-    printf("----------PASO 2----------\n");
-    printf("----------PROCESANDO DATOS----------\n");
+    imprimirMensaje("----------PASO 2----------\n");
+    imprimirMensaje("----------PROCESANDO DATOS----------\n");
 	float suma = calcularSuma(numero1, numero2);
-    printf("----------PASO 3----------\n");
-    printf("----------SALIDAS DE DATOS----------\n");
+    imprimirMensaje("----------PASO 3----------\n");
+    imprimirMensaje("----------SALIDAS DE DATOS----------\n");
 	imprimirFloat("El resultado de la suma es: ",suma);
-    printf("----------FIN DEL PROGRAMA----------\n");
-    printf("--------------------------------------\n");
+    imprimirMensaje("----------FIN DEL PROGRAMA----------\n");
+    imprimirMensaje("--------------------------------------\n");
 	
     return 0;
 }
